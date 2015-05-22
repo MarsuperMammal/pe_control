6b59b0354b2b85d8f470d9cab326d41374ed2b63
Phil Watts <pwatts217@gmail.com>
2015-05-22 12:04:08 -0400

    added .ssh directories and public keys to profiles base

--- dist/profiles/manifests/base.pp
+++ dist/profiles/manifests/base.pp
@@ -17,10 +17,16 @@ class profiles::base {
     shell          => '/bin/zsh',
     purge_ssh_keys => true,
   }->
+  file { '/home/pwatts/.ssh':
+    ensure => 'directory',
+    owner  => 'pwatts',
+    group  => 'pwatts',
+    mode   => '0500',
+  }->
   ssh_authorized_key { 'pwatts217@gmail.com':
     user => 'pwatts',
     type => 'ssh-rsa',
-    key  => $pwatts_id_rsa,
+    key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDYW7nMpUo/aFq1+Ch5u1iv/vZJ73ymF61JJ3IL/Km6TC2gZy5wCQjT9eQkgcPmDDva+clIMgbMiVEA0HRr+DUisXZcedvS+xcx89zplNiPjPlSNJS6nUh6Ui35whM8betjpo9NwfILnSyvp+wueHQzEie/blxq9FgXvUXV7MoLsUkATiLruhoNHO0jYdJ1pmNLvm9p21ePdF08jbEfD691ohTGHSjRZmyCdHjfkYTTrrP9ay/6htxsPuO+9SFPfHPiSAGDByghrcV7JAZWNCFRai5dBTCTPlB+ChlDKDy4HZ2umtguUWuvpRo04rAtoB07kYy3/Z1Joog6aQgzWPEz',
   }
 
   group {'aharden':
@@ -33,9 +39,15 @@ class profiles::base {
     shell  => '/bin/zsh',
     purge_ssh_keys => true,
   }->
+  file { '/home/aharden/.ssh':
+    ensure => 'directory',
+    owner  => 'aharden',
+    group  => 'aharden',
+    mode   => '0500',
+  }->
   ssh_authorized_key { 'aharden@te.com':
     user => 'aharden',
     type => 'ssh-rsa',
-    key  => $aharden_id_rsa,
+    key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDTuLisAYwu72OAfh1/SxvqZgEQ2IW3/3nlBm4LMIPRpVg2tRFCZ5UwmaL8+VW4mNs/tlXLkyYA+lDVe4jJcbzvQ8ZC1d2pK/gwN7JGjle/Ui4UzBpFT19AMrMQ6KCOGOgqwunQuzMTtth89QxCYFvNwIuchU5uB4V7rvX2KOyCrPlxIt1fxYBObyhS6eiW322GiOX/3Boo43DwMSIGBlTfBAsolHypZtJcEN0hQonKifB51Y7+s6WwQtd19P6Fxn1kg9aUo5JxRW4IWNKnuQ/KB6HdmyWx5mE3MyC826Zgv0q3jVWYLw2txaVFM9KgoZ5kmwe593cK2Hev9CSuV1iD',
   }
 }
