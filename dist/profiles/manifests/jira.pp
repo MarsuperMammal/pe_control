ed8f7b49c0a371b62ccc7d265693d9a17cca776e
Phil Watts <pwatts217@gmail.com>
2015-05-21 22:01:06 -0400

    added jira hieradata

--- dist/profiles/manifests/jira.pp
+++ dist/profiles/manifests/jira.pp
@@ -1,3 +1,22 @@
-class profiles::jira {
+class profiles::jira (
+  $jiradb_pass
+) {
   include jira
+
+  class {'postgres::server': }
+
+  postgres::server::db { 'jira':
+    user     => 'jiraadm',
+    password => $jiradb_pass,
+  }
+ 
+  group { 'jira':
+    ensure => 'present',
+  }
+
+  user { 'jira':
+    ensure => 'present',
+    groups => 'jira',
+  }
+
 }
