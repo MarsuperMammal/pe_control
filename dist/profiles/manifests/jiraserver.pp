2995f75b6f8fbf02d4a5efbf0e44f5763b278aad
Phil Watts <pwatts217@gmail.com>
2015-05-22 10:30:59 -0400

    added base directory control

--- dist/profiles/manifests/jiraserver.pp
+++ dist/profiles/manifests/jiraserver.pp
@@ -5,6 +5,13 @@ class profiles::jiraserver (
 
   class {'postgresql::server': }
 
+  file { '/opt/atlassian':
+    ensure => 'directory',
+    owner  => 'jira',
+    group  => 'jira',
+    mode   => '0755',
+  }
+
   postgresql::server::db { 'jira':
     user     => 'jiraadm',
     password => $jiradb_pass,
