71ce7df2b61c6e44af9dabba826348a967b78a63
Phil Watts <pwatts217@gmail.com>
2015-05-22 11:38:40 -0400

    added ssh keys and users to puppet control

--- dist/profiles/manifests/base.pp
+++ dist/profiles/manifests/base.pp
@@ -6,4 +6,30 @@ class profiles::base {
     default : {
     }
   }
+
+  user { 'pwatts':
+    ensure         => 'present',
+    group          => 'pwatts',
+    home           => '/home/pwatts',
+    shell          => '/bin/zsh',
+    purge_ssh_keys => true,
+  }->
+  ssh_authorized_key { 'pwatts217@gmail.com':
+    user => 'pwatts',
+    type => 'ssh-rsa',
+    key  => $pwatts_id_rsa,
+  }
+
+  user { 'aharden':
+    ensure => 'present',
+    group  => 'aharden',
+    home   => '/home/aharden',
+    shell  => '/bin/zsh',
+    purge_ssh_keys => true,
+  }->
+  ssh_authorized_key { 'aharden@te.com':
+    user => 'aharden',
+    type => 'ssh-rsa',
+    key  => $aharden_ssh_key,
+  }
 }
