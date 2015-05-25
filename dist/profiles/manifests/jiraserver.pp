8aa1bbd474d113427e5a685275ae5e5982969077
Alex Harden <aharden@te.com>
2015-05-25 11:26:22 -0400

    added apache SSL termination and java mgmt

--- dist/profiles/manifests/jiraserver.pp
+++ dist/profiles/manifests/jiraserver.pp
@@ -1,7 +1,71 @@
 class profiles::jiraserver (
-  $jiradb_pass
+  $certificate,
+  $jiradb_pass,
+  $private_key,
 ) {
-  include jira
+  include ::java
+  include ::jira
+  include ::jira::facts
+
+  $cert_file = "/etc/ssl/certs/${::fqdn}.crt.pem"
+  $key_file  = "/etc/ssl/private/${::fqdn}.key.pem"
+
+  File {
+    ensure => present,
+    owner  => 'root',
+  }
+
+  class { 'apache':
+    mpm_module    => 'prefork',
+    default_vhost => false,
+  }
+
+  class { 'apache::mod::rewrite':
+  }
+
+  class { 'apache::mod::ssl':
+  }
+
+  apache::vhost { 'jira non-ssl':
+    servername => 'jira',
+    port       => '80',
+    docroot    => '/var/www',
+    rewrites   => [
+      {
+        comment      => 'redirect to https',
+        rewrite_cond => ['%{HTTPS} off'],
+        rewrite_rule => ['(.*) https://%{HTTP_HOST}:443%{REQUEST_URI}'],
+      },
+    ],
+  }
+
+  apache::vhost { 'jira ssl':
+    servername          => 'jira',
+    port                => '443',
+    docroot             => '/var/www',
+    default_vhost       => true,
+    ssl                 => true,
+    ssl_cert            => $cert_file,
+    ssl_key             => $key_file,
+    proxy_preserve_host => 'On',
+    proxy_pass          => [ {
+      'path' => '/',
+      'url'  => 'http://localhost:8080/' },
+    ],
+    require             => Apache::Vhost [ 'jira non-ssl']
+  }
+
+  file { $cert_file:
+    group   => 'root',
+    mode    => '0644',
+    content => template("${module_name}/jira/crt.pem.erb"),
+  }
+
+  file { $key_file:
+    group   => 'ssl-cert',
+    mode    => '0400',
+    content => template("${module_name}/jira/key.pem.erb"),
+  }
 
   class {'postgresql::server': }
 
