2cbce578228a81afba6208bc5a53ea5d25e88ee3
Phil Watts <pwatts217@gmail.com>
2015-05-24 22:01:33 -0400

    fixed syntax issue

--- dist/profiles/manifests/teamcityagent.pp
+++ dist/profiles/manifests/teamcityagent.pp
@@ -2,7 +2,7 @@ class profiles::teamcityagent {
   Rvm_gem {
     ruby_version => 'ruby-2.0.0-p645',
     ensure       => 'latest',
-    require      => Rvm_sytem_ruby ['ruby-2.0.0-p645'],
+    require      => Rvm_sytem_ruby['ruby-2.0.0-p645'],
   }
 
   rvm_gem { $gemlist: }
