3fb4abbcd11e3ca2b44db375426ade611ee033e8
Phil Watts <pwatts217@gmail.com>
2015-05-25 15:11:28 -0400

    Added jira redirection with proxy termination

--- dist/profiles/manifests/jiraserver.pp
+++ dist/profiles/manifests/jiraserver.pp
@@ -1,7 +1,9 @@
+# Jira Server configuration
 class profiles::jiraserver {
-  include jira
+  include ::jira
 
-  class {'postgresql::server': }
+  $cert_file = "/etc/ssl/certs/${::fqdn}.crt.pem"
+  $key_file  = "/etc/ssl/private/${::fqdn}.key.pem"
 
   file { '/opt/atlassian':
     ensure => 'directory',
@@ -9,6 +11,62 @@ class profiles::jiraserver {
     group  => 'jira',
     mode   => '0755',
   }
+
+  file { $cert_file:
+    ensure  => 'present',
+    user    => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template("${module_name}/jira/cert.pem.erb"),
+  }
+
+  file { $key_file:
+    ensure  => 'present',
+    user    => 'root',
+    group   => 'ssl-cert',
+    mode    => '0400',
+    content => template("${module_name}/jira/key.pem.erb"),
+  }
+
+
+  class { 'apache':
+    mpm_module    => 'prefork',
+    default_vhost => false,
+  }
+
+  class { 'apache::mod::rewrite': }
+  class { 'apache::mod::ssl':     }
+  class { 'postgresql::server':   }
+
+  apache::vhost { 'jira non-ssl':
+    servername => 'jira',
+    port       => '80',
+    docroot    => '/var/www',
+    rewrites   => [
+      {
+        comment      => 'redirect to https',
+        rewrite_cond => ['%{HTTPS} off'],
+        rewrite_rule => '(.*) https://%{HTTP_HOST}:443%{REQUEST_URI}'],
+      },
+    ],
+  }
+
+  apache::vhost { 'jira ssl':
+    servername          => 'jira',
+    port                => '443',
+    docroot             => '/var/www',
+    default_vhost       => true,
+    ssl                 => true,
+    proxy_preserve_host => 'On',
+    proxy_pass          => [
+      {
+        path => '/',
+        url  => 'http://localhost:8080/',
+      },
+    ],
+    require             => Apache::Vhost ['jira non-ssl'],
+  }
+
 
   postgresql::server::db { 'jira':
     user     => 'jiraadm',
