ad15dd34185d7ca78f31097c42b18ba371236728
Phil Watts <pwatts217@gmail.com>
2015-05-27 15:29:47 -0400

    added prereceive hook to stash configurations

--- dist/profiles/manifests/stashserver.pp
+++ dist/profiles/manifests/stashserver.pp
@@ -1,17 +1,27 @@
+# Definition for stash server
 class profiles::stashserver {
   include stash
 
-  class { 'postgresql::server' : }
-
-  file { ['/opt/atlassian','/opt/atlassian/application-data/'] :
-    ensure => 'directory',
+  File {
     owner  => 'stash',
     group  => 'stash',
     mode   => '0755',
   }
 
+  class { 'postgresql::server' : }
+
+  file { ['/opt/atlassian','/opt/atlassian/application-data/',\
+  '/external-hooks'] :
+    ensure => 'directory',
+  }
+
   postgresql::server::db { 'stash':
     user     => 'stash',
     password => hiera('stashdb_pass'),
+  }
+
+  file { '/external-hooks/puppet_prereceive.sh':
+    ensure => 'file',
+    source => "puppet:///${module_name}/puppet_prereceive.sh",
   }
 }
